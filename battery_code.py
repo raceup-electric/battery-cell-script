2ad80731b3aeaaab153e4ac2aa4df1e7ccbd7070
Filippo Cracco <83571047+filippocracco@users.noreply.github.com>
2025-12-16 16:25:55 +0100

    Refactor battery_code.py for new settings and data
    Updated CSV and log file names, adjusted minimum voltage, and modified data reading and logging to include additional temperature readings.

--- battery_code.py
+++ battery_code.py
@@ -10,10 +10,10 @@ import asyncio
 # =========================
 # SETTINGS
 # =========================
-CSV_FILE = "test.csv"
-LOG_FILE = "charge_cell_log.xlsx"
+CSV_FILE = "square_wave.csv"
+LOG_FILE = "test_sorato_6_log.xlsx"
 COM_PORT = "COM6"
-MIN_VOLT = 0.0
+MIN_VOLT = 3.0
 MAX_VOLT = 4.3
 
 
@@ -21,7 +21,7 @@ MAX_VOLT = 4.3
 wb = openpyxl.Workbook()
 ws = wb.active
 ws.title = "Log"
-ws.append(["Index", "Time (s)", "Voltage (V)", "Current setpoint (A)", "Current (A)", "Ah", "Wh", "Weight (kg)"])
+ws.append(["Index", "Time (s)", "Voltage (V)", "Setpoint (A)", "Current (A)", "Ah", "Wh", "Weight (kg)", "T1 (°C)", "T2 (°C)", "T3 (°C)", "T4 (°C)", "T5 (°C)"])
 
 
 # POWER SUPPLY
@@ -75,7 +75,7 @@ def read_data(start_time, row_index, task, setpoint_current):
     t = time.time() - start_time
     
     v = float(inst.query("FETC:SCAL:VOLT?"))
-    i = abs(float(inst.query("FETC:SCAL:CURR?")))
+    i = float(inst.query("FETC:SCAL:CURR?"))
     Ah = float(inst.query("FETC:AHO?"))
     Wh = float(inst.query("FETC:WHO?"))
     
@@ -83,22 +83,22 @@ def read_data(start_time, row_index, task, setpoint_current):
     sample = [s[0] for s in sample]
     
     voltage = float(sample[0])
-    kg = voltage * 500000
+    pressure_kg = voltage * 500000
     temp1 = float(sample[1])
     temp2 = float(sample[2])
     temp3 = float(sample[3])
     temp4 = float(sample[4])
     temp5 = float(sample[5])
-    pressure_kg = voltage * 500000
+    temp6 = float(sample[6])
     
-    print(f"\r#{row_index} | t={t:.1f}s | V={v:.2f} V | I={i:.3f} A | Ah={Ah:.3f} | Wh={Wh:.3f} | kg={pressure_kg:.3f} | temp1={temp1:.3f} | temp2={temp2:.3f}", end="") #DA AGGIUNGERE DATI
-    return [row_index, t, v, setpoint_current, i, Ah, Wh, pressure_kg] #DA AGGIUNGERE DATI
+    print(f"\r#{row_index} | t={t:.1f}s | V={v:.2f} V | I={i:.3f} A | Ah={Ah:.3f} | Wh={Wh:.3f} | kg={pressure_kg:.3f} | t1={temp1:.3f} | t2={temp2:.3f} | t3={temp3:.3f} | t4={temp4:.3f} | t5={temp5:.3f} | Tambient={temp6:.3f}", end="")
+    return [row_index, t, v, setpoint_current, i, Ah, Wh, pressure_kg, temp1, temp2, temp3, temp4, temp5, temp6]
 
 async def v_integrity_check(stop_trigger):
     while not stop_trigger.is_set():
         v = float(inst.query("FETC:SCAL:VOLT?"))
-        if v >= MAX_VOLT or v <= MIN_VOLT:
-            print("\nVoltage limit reached (V = {v:.2f}), stopping step early.")
+        if v > MAX_VOLT or v < MIN_VOLT:
+            print(f"\nVoltage limit reached (V = {v:.2f}), stopping step early.", end="")
             stop_trigger.set()
 
         await asyncio.sleep(0.01)
@@ -112,7 +112,7 @@ async def setpoint_handler(stop_trigger, pause_trigger, setpoint_current):
                 await pause_trigger.wait()   # wait until restart
 
             async with setpoint_current.lock:
-                setpoint_current.value = int(line[1])
+                setpoint_current.value = float(line[1])
                 setpoint_time = float(line[0])
 
                 print(f"\nApplying I = {setpoint_current.value} A for {setpoint_time} seconds...")
@@ -130,7 +130,11 @@ async def logger(stop_trigger, task, setpoint_current):
     while not stop_trigger.is_set():
 
         async with setpoint_current.lock:
-            data = read_data(start_time, row_index, task, setpoint_current.value)
+            
+            try:
+                data = read_data(start_time, row_index, task, setpoint_current.value)
+            except Exception as e:
+                print(f"\nErrore lettura dati: {e}")
 
             # Write Excel
             ws.append(data)
@@ -138,7 +142,7 @@ async def logger(stop_trigger, task, setpoint_current):
 
             # Save periodically
             save_counter += 1
-            if save_counter >= 200:
+            if save_counter >= 100:
                 wb.save(LOG_FILE)
                 save_counter = 0
 
@@ -153,7 +157,6 @@ async def user_input(stop_trigger, pause_trigger):
             print("Script is having a break...")
         elif cmd == "r":
             pause_trigger.set()
-            inst.write("OUTP 1")
             print("Script is starting again...")
         elif cmd == "q":
             stop_trigger.set()
@@ -172,20 +175,23 @@ async def main():
         loadcell = task.ai_channels.add_ai_bridge_chan("cDAQ1Mod1/ai0", min_val = -0.05, max_val = 0.05, bridge_config = BridgeConfiguration.FULL_BRIDGE, nominal_bridge_resistance=350)
         loadcell.ai_adc_timing_mode = ADCTimingMode.BEST_50_HZ_REJECTION
         
-        canale1 = task.ai_channels.add_ai_thrmcpl_chan("cDAQ1Mod2/ai0", min_val = 0.0, max_val = 100.0, units = TemperatureUnits.DEG_C, thermocouple_type = ThermocoupleType.J)
+        canale1 = task.ai_channels.add_ai_thrmcpl_chan("cDAQ1Mod2/ai0", min_val = 0.0, max_val = 100.0, units = TemperatureUnits.DEG_C, thermocouple_type = ThermocoupleType.K)
         canale1.ai_adc_timing_mode = ADCTimingMode.HIGH_RESOLUTION
         
-        canale2 = task.ai_channels.add_ai_thrmcpl_chan("cDAQ1Mod2/ai1", min_val = 0.0, max_val = 100.0, units = TemperatureUnits.DEG_C, thermocouple_type = ThermocoupleType.J)
+        canale2 = task.ai_channels.add_ai_thrmcpl_chan("cDAQ1Mod2/ai1", min_val = 0.0, max_val = 100.0, units = TemperatureUnits.DEG_C, thermocouple_type = ThermocoupleType.K)
         canale2.ai_adc_timing_mode = ADCTimingMode.HIGH_RESOLUTION
         
-        canale3 = task.ai_channels.add_ai_thrmcpl_chan("cDAQ1Mod2/ai2", min_val = 0.0, max_val = 100.0, units = TemperatureUnits.DEG_C, thermocouple_type = ThermocoupleType.J)
+        canale3 = task.ai_channels.add_ai_thrmcpl_chan("cDAQ1Mod2/ai2", min_val = 0.0, max_val = 100.0, units = TemperatureUnits.DEG_C, thermocouple_type = ThermocoupleType.K)
         canale3.ai_adc_timing_mode = ADCTimingMode.HIGH_RESOLUTION
         
-        canale4 = task.ai_channels.add_ai_thrmcpl_chan("cDAQ1Mod2/ai3", min_val = 0.0, max_val = 100.0, units = TemperatureUnits.DEG_C, thermocouple_type = ThermocoupleType.J)
+        canale4 = task.ai_channels.add_ai_thrmcpl_chan("cDAQ1Mod2/ai3", min_val = 0.0, max_val = 100.0, units = TemperatureUnits.DEG_C, thermocouple_type = ThermocoupleType.K)
         canale4.ai_adc_timing_mode = ADCTimingMode.HIGH_RESOLUTION
         
-        canale5 = task.ai_channels.add_ai_thrmcpl_chan("cDAQ1Mod2/ai4", min_val = 0.0, max_val = 100.0, units = TemperatureUnits.DEG_C, thermocouple_type = ThermocoupleType.J)
+        canale5 = task.ai_channels.add_ai_thrmcpl_chan("cDAQ1Mod2/ai4", min_val = 0.0, max_val = 100.0, units = TemperatureUnits.DEG_C, thermocouple_type = ThermocoupleType.K)
         canale5.ai_adc_timing_mode = ADCTimingMode.HIGH_RESOLUTION
+        
+        canale6 = task.ai_channels.add_ai_thrmcpl_chan("cDAQ1Mod2/ai5", min_val = 0.0, max_val = 100.0, units = TemperatureUnits.DEG_C, thermocouple_type = ThermocoupleType.J)
+        canale6.ai_adc_timing_mode = ADCTimingMode.HIGH_RESOLUTION
         
         task.timing.cfg_samp_clk_timing(10.0, sample_mode=AcquisitionType.CONTINUOUS)
         task.start()
@@ -203,14 +209,16 @@ async def main():
 
         except KeyboardInterrupt:
             print("\n*** Manual STOP ***")
-            stop_trigger.set()        
+            stop_trigger.set()
+        except Exception as e:
+            print(f"\n*** ERROR OCCURRED: {e} ***")
+            stop_trigger.set()
         finally:
             # Task deletion procedure
             for t in tasks:
                 t.cancel()           
             await asyncio.gather(*tasks, return_exceptions=True)
             inst.write("OUTP 0")
-            inst.close()
             wb.save(LOG_FILE)        
             print(f"\nSaved to {LOG_FILE}")
             print("\n=== DONE ===")
